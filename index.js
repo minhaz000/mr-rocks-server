bf76b0ce9bcf2ad1938f663a1cd0bf1e05d957df
minhaz000 <mrmminhaz@gamail.com>
2022-10-25 18:03:55 +0600

    get route set

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ app.use(cors())
 app.get('/', (req, res) => {
   res.send('wellcome to assingment 10  server backend' )
 })
-
+app.get('/courses', (req, res) => { 
+ res.send(courses)
+})
+app.get('/courses/:id', (req, res) => { 
+  const  id  = req.params.id 
+   const course = courses.data.find( c => c.id === id )
+  res.send( course)
+})
 // =========================================================
 
 // =========================================================
