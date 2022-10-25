81b7876d201125ca4d29c8a2d6cd9a66d9b213ee
minhaz000 <mrmminhaz@gamail.com>
2022-10-25 17:43:52 +0600

    corse set up done

--- index.js
+++ index.js
@@ -11,14 +11,7 @@ app.use(cors())
 app.get('/', (req, res) => {
   res.send('wellcome to assingment 10  server backend' )
 })
-app.get('/courses', (req, res) => { 
- res.send(courses)
-})
-app.get('/courses/:id', (req, res) => { 
-  const  id  = req.params.id 
-   const course = courses.data.find( c => c.id === id )
-  res.send( course)
-})
+
 // =========================================================
 
 // =========================================================
